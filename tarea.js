05b41b669622779d6d40d87c6c7131bbc2479d6c
machicado <machicadogomezalexander@gmail.com>
2022-11-18 09:10:55 -0500

    primer agregado

---
+++ tarea.js
@@ -0,0 +1,35 @@
+
+//EJERCICIO-1--------------------------------------------------------------------
+// let multipli = function(x,y){
+//   return x*y
+// }
+// let expon=function(x,y){
+//   return Math.pow(x,y)
+// }
+// let suma=(x,y)=>{
+//   return x+y
+// }
+// let resta=(x,y)=>{
+//   return x-y
+// }
+// let resultado=resta(10,8)
+// alert(resultado)
+
+//EJERCICIO-2--------------------------------------------------------------------
+let farenheit=(grado)=>{
+  return farenheit-grado*1.8+32;
+}
+let celcius=(grado)=>{
+  return (grado-32)*5/9
+}
+let resultado2=celcius(49)
+alert(resultado2)
+
+//EJERCICIO-3--------------------------------------------------------------------
+// let sumar=(a,b)=>{
+//   return a+b
+// }
+// let a=parseFloat(prompt("ejercicio #3 ingrese un numero"))
+// let b=parseFloat(prompt("ejercicio #3 ingrese otro numero"))
+// let resultado3=sumar(a,b)
+// alert(resultado3)
